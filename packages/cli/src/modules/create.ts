ace842dffa769a062ebee5a98e9dcf44ae841a97
trobonox <57040351+trobonox@users.noreply.github.com>
2024-07-18 22:48:12 +0200

    feat: add initial template dl with giget

--- packages/cli/src/modules/create.ts
+++ packages/cli/src/modules/create.ts
@@ -1,3 +1,11 @@
+import { downloadTemplate } from "giget";
+
 export const createModule = async (str: any, options: any) => {
     // Logic for creating app
+    const bareTemplateLink = "github:stein-js/stein/examples/bare";
+
+    const { source, dir } = await downloadTemplate(bareTemplateLink, {
+        force: true,
+        dir: ".",
+    });
 }
