1765c3df5754b7ff088f68b3c436b185cd0edcec
Mikkel RINGAUD <mikkel.almonteringaud@legrand.com>
2024-07-19 10:08:17 +0200

    feat(core): add support for plugins

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,13 +1,40 @@
-import { createServer } from "vite";
+import { createServer, PluginOption as VitePluginOption } from "vite";
 import solid from "vite-plugin-solid";
 import type { PartialDeep } from "type-fest";
 
 export const startDevelopmentServer = async (cwd: string, config: SteinConfig): Promise<void> => {
-  const server = await createServer({
-    plugins: [solid()],
-    root: cwd,
+  let solidIndex = 0;
+  const plugins: VitePluginOption = [solid()];
 
-    clearScreen: false,
+  for (const plugin of config.plugins) {
+    // We need to register the plugins that were made for Vite.
+    for (const vitePlugin of plugin.extends ?? []) {
+      if (!vitePlugin) continue; // skip if false or undefined.
+
+      // position is given, put it in the right place.
+      if ("position" in vitePlugin) {
+        if (vitePlugin.position === "before-solid") {
+          plugins.splice(solidIndex, 0, vitePlugin.plugin);
+          solidIndex++;
+        }
+        else plugins.push(vitePlugin.plugin);
+      }
+      else plugins.push(vitePlugin);
+    }
+
+    if (plugin.vite) {
+      plugins.push(plugin.vite);
+      console.info("registered a stein main vite plugin for:", plugin.name);
+    }
+    
+    console.info("registered a stein plugin:", plugin.name);
+  }
+  
+  const server = await createServer({
+    plugins,
+    
+    root: cwd,
+    clearScreen: false
   });
   
   await server.listen(config.development.port);
@@ -15,16 +42,35 @@ export const startDevelopmentServer = async (cwd: string, config: SteinConfig): 
 };
 
 export interface SteinConfig {
+  plugins: Plugin[];
+
   development: {
     port: number;
   }
 }
 
 export const defineConfig = (options: PartialDeep<SteinConfig>): SteinConfig => ({
+  plugins: [],
   ...options,
-
+  
   development: {
     port: 3000,
     ...options.development
   }
 });
+
+export interface Plugin {
+  name: string
+
+  /** Vite plugins involved in this process. */
+  extends?: Array<VitePluginOption | {
+    /** @default "after-solid" */
+    position: "before-solid" | "after-solid",
+    plugin: VitePluginOption
+  }>
+  
+  vite?: VitePluginOption
+}
+
+/** Helper to have types when making a new plugin. */
+export const definePlugin = <T>(plugin: (config?: PartialDeep<T>) => Plugin) => plugin;
