d8855bc0461d1460a7d37475832aa11984c9fabd
Mikkel RINGAUD <mikkel.almonteringaud@legrand.com>
2024-07-19 15:32:08 +0200

    chore(examples): add unocss/reset

--- examples/bare/stein.config.ts
+++ examples/bare/stein.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@steinjs/core";
 import unocss from "stein-plugin-unocss";
 
 export default defineConfig({
-  plugins: [unocss()],
+  plugins: [unocss({ injectReset: true })],
   
   development: {
     port: 1234
