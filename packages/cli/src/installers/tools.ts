6d6204ffed8fc7e1d70b7bbcaa2602c856788135
trobonox <57040351+trobonox@users.noreply.github.com>
2024-07-21 16:33:45 +0200

    chore(cli): add initial tool installer boilerplate

---
+++ packages/cli/src/installers/tools.ts
@@ -0,0 +1,9 @@
+const AVAILABLE_TOOLS = ['biome', 'eslint', 'prettier'];
+
+export const installSteinTool = async (tool: string, projectDir: string) => {
+    if (!AVAILABLE_TOOLS.includes(tool)) {
+        throw new Error(`Tool ${tool} is not a valid stein plugin`);
+    }
+
+
+}
