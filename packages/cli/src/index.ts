5fc85a452ba3ec736ea2804f50f48fed3b62a45c
trobonox <57040351+trobonox@users.noreply.github.com>
2024-07-23 15:17:39 +0200

    fix(cli): only run CLI parser if command is called from CLI

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,30 +1,41 @@
 #!/usr/bin/env node
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { Command } from "commander";
 import { version } from "../package.json";
 import { buildModule } from "./modules/build";
 import { createModule } from "./modules/create";
 import { devModule } from "./modules/dev";
 
-const program = new Command();
+export { createModule }; // export for usage in create-stein
 
-program
-  .name("stein")
-  .description("CLI to run projects and scaffold a new stein project")
-  .version(version);
+const pathToThisFile = resolve(fileURLToPath(import.meta.url));
+const pathPassedToNode = resolve(process.argv[1]);
+const isThisFileBeingRunViaCLI = pathToThisFile.includes(pathPassedToNode);
 
-program
-  .command("dev")
-  .description("start the stein development server")
-  .action(devModule);
+if (isThisFileBeingRunViaCLI) {
+  const program = new Command();
 
-program
-  .command("build")
-  .description("make a stein production build")
-  .action(buildModule);
+  program
+    .name("stein")
+    .description("CLI to run projects and scaffold a new stein project")
+    .version(version);
 
-program
-  .command("create")
-  .description("scaffold a new stein project")
-  .action(createModule);
+  program
+    .command("dev")
+    .description("start the stein development server")
+    .action(devModule);
 
-program.parse();
+  program
+    .command("build")
+    .description("make a stein production build")
+    .action(buildModule);
+
+  program
+    .command("create")
+    .description("scaffold a new stein project")
+    .action(createModule);
+
+  program.parse();
+}
